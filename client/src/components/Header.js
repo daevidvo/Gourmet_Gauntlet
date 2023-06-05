36f998ae340b77c26de6fb7c79a41de0d91bd1f0
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-06-04 19:10:14 -0700

    moved from tailwind to bulma for frameworks and updated header and footer

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,58 +4,46 @@ import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 
 function Header() {
-    const navigationItems = [
-        { title: 'About Us', link: '/About-Us' },
-    ];
-
-    const location = useLocation();
-
     return (
-        <header className="flex items-center justify-center px-4 py-2 bg-gray-600 h-16">
-            <div className="flex items-center">
-                <img src={myIcon} alt="Company Icon" className="my-icon w-20 h-18" />
-            </div>
-            <nav>
-                <ul className="nav-bar flex ">
-                    {navigationItems.map((item, index) => (
-                        <li key={index} className={location.pathname === item.link ? 'active' : ''}>
-                            <Link
-                                to={item.link}
-                                className="mx-3 text-white font-semibold py-5">
-                                <div className="px-5 hover:bg-gray-200 py-4 hover:opacity-75 hover:text-black rounded transition duration-200 transform hover:scale-110">
-                                    {item.title}
-                                </div>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                <ul className="flex">
-                    {Auth.loggedIn() ? (
-                        <>
-                            <li className="mx-3 text-white font-semibold py-5">
-                                <div className="px-5 hover:bg-gray-200 py-4 hover:opacity-75 hover:text-black rounded transition duration-200 transform hover:scale-110">
-                                    <Link to="/Home">Home</Link>
-                                </div>
-                            </li>
-                        </>
-                    ) : (
-                        <>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                </a>
 
-                            <li className="mx-3 text-white font-semibold py-5">
-                                <div className="px-5 hover:bg-gray-200 py-4 hover:opacity-75 hover:text-black rounded transition duration-200 transform hover:scale-110">
-                                    <Link to="/Login">Login</Link>
-                                </div>
-                            </li>
-                            <li className="mx-3 text-white font-semibold py-5">
-                                <div className="px-5 hover:bg-gray-200 py-4 hover:opacity-75 hover:text-black rounded transition duration-200 transform hover:scale-110">
-                                    <Link to="/Signup">Signup</Link>
-                                </div>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </nav>
-        </header>
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <Link className="navbar-item" to="/">
+                        <a>
+                            Home
+                        </a>
+                    </Link>
+                    <a className="navbar-item" href="https://github.com/daevidvo/Gourmet_Gauntlet">
+                        Documentation
+                    </a>
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button is-primary">
+                                <strong>Sign up</strong>
+                            </a>
+                            <a className="button is-light">
+                                Log in
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 }
 
