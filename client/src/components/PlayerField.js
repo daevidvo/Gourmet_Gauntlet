07eccaa9ced99354a2c3e0ee105c60fc09d62a0e
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-06-06 15:29:47 -0600

    started playerField

--- client/src/components/PlayerField.js
+++ client/src/components/PlayerField.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useMutation } from "@apollo/client";
 import { useQuery } from "@apollo/client";
-import gameCards from "../components/gameCards"
 import { GET_PLAYER_CARDS, GET_HEALTH, GET_STAGE } from "../utils/queries";
 import { SET_GAME_CARDS, SET_GAME_HEALTH, SET_GAME_STAGE } from "../utils/mutations";
 
@@ -13,6 +12,11 @@ export const PlayerField = () => {
     const { loading: loadingPlayerCards, data: PCards } = useQuery(GET_PLAYER_CARDS);
     const { loading: loadingPlayerHealth, data: PHealth } = useQuery(GET_HEALTH);
     const { loading: loadingPlayerStage, data: PStage } = useQuery(GET_STAGE);
+
+    const playerCards = PCards?.getPlayerCards.userCards || [];
+    const playerHealth = PHealth?.getHealth.userHealth;
+    const playerStage = PStage?.getStage.userStage;
+
 
     return (
         <>
