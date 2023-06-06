1588d011e9fe2b847dfeb0e862744744e5eb59db
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-06-05 20:24:19 -0600

    added draw cards on load and rendering

--- client/src/pages/Battle.js
+++ client/src/pages/Battle.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useQuery, useMutation } from "@apollo/client";
+import Card from '../components/Card';
+import { GET_ALL_CARDS, GET_GAME } from "../utils/queries";
 
 /* 
 On page load:
@@ -11,15 +13,16 @@ On page load:
             stage number
     query Cards
 
-while (healh > 0)
+while (userHealh > 0)
 {
 Draw phase:
     draw 5 cards (math rand/floor)
     allow user to assign cards to field 
 
 On click battle button:
+Load enemy cards
 (do-while loop condition h1 > 0 && h2 > 0)
-    Subtract a1 from h2 && a2 from h1 
+    Subtract a1 from h2 && a2 from h1 (with timeout)
         If either h is negative or 0, that card dies (either bounces or grays and disappears)
             The cards after the dead card slide forward to fill the slot (if slot zero is first)
             If enemy has cards; player does not
@@ -27,16 +30,47 @@ On click battle button:
             If player has; enemy does not
                 player wins; next enemy
         If neither h is negative, repeat the process 
+
+Post-battle:
+    update Game information
+        userHealth
+        userStage
+        userCards
 }
 */
 
-
-const Battle = () => {
-
+const Battle =  () => {
+    const { loading, data } = useQuery(GET_ALL_CARDS);
+    const cards = data?.getAllCards;
+    console.log(data);
+    console.log(cards);
     
+    if(loading){
+        return (
+            <h1> Loading </h1>
+        )
+    }
+
+    let randArray = [];
+    const fiveRandCards = () => {
+        randArray = [];
+        for (let i = 0; i < 5; i++){
+            let randNum = Math.floor(Math.random() * 14); //5 rand cards from 14 card db
+            randArray.push(cards[randNum]);
+        }
+        console.log(randArray);
+
+        return null;
+    }
+
+    fiveRandCards();
 
 
-
+return (
+    <div className="is-flex">
+        {randArray.map((cardData) => <Card {...cardData} />)}
+    </div>
+)
 }
 
 export default Battle
