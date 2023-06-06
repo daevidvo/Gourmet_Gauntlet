85dd7c2ffeff6aa042903bdcc60b34de1d056f09
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-06-06 12:41:16 -0600

    changed coloring and header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 import { GET_ME } from "../utils/queries";
 
+const GGName = require('../assets/GGname.png');
+
 const HomePage = () => {
   const { loading, data } = useQuery(GET_ME);
   const loggedIn = data?.me ? true : false;
@@ -17,22 +19,28 @@ const HomePage = () => {
     return loggedIn ? (
       <>
         <p className="title is-4">Welcome, {user}!</p>
-        <Link to="/battle" className="button is-primary is-large">
+        <Link to="/battle" className="button is-warning is-large">
           Battle
         </Link>
       </>
     ) : (
-      <Link to="/login" className="button is-primary is-large">
+      <Link to="/login" className="button is-warning is-large">
         Login/Signup
       </Link>
     );
   };
 
   return (
-    <section className="hero is-primary is-fullheight">
+    <section className="hero is-danger is-fullheight">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h1 className="title is-1">Gourmet Gauntlet</h1>
+          <h1 className="title is-1">
+          <img
+                src={GGName}
+                // width="100"
+                // height="100"
+            />
+          </h1>
           {renderButton()}
         </div>
       </div>
