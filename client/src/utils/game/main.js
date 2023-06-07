71ece73eda7a291f6c089a264a898d46d57e5843
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-06-06 20:43:07 -0700

    updated field display on main

--- client/src/utils/game/main.js
+++ client/src/utils/game/main.js
@@ -1,11 +1,11 @@
-import drawCards from "./drawCards"
-import { handleAssignCards } from "./handleAssignCards";
+import drawCards from "./drawCards";
 import createCardElements from "./createCardElements";
 
 
 export default async function playGame() {
     let game = 'draw'
     let handArr = [];
+    let fieldArray = [];
 
     const gameView = document.getElementById('battle')
 
@@ -16,9 +16,12 @@ export default async function playGame() {
 
     // once card is selected it is removed from handArr and added to fieldArr
     const selectCard = (card) => {
-        handleAssignCards(handArr, card);
-        const selectedCard = document.getElementById(card.id);
-        selectedCard.remove();
+        if (fieldArray.length < 4) {
+            fieldArray.push(card);
+            handArr.splice(handArr.indexOf(card), 1);
+        }
+        // remove HTML card from displayed hand
+        card.remove();
     };
 
     // button to switch to battle phase
@@ -26,11 +29,20 @@ export default async function playGame() {
         game = "battle";
     };
 
+    // create view of field above hand
+    const fieldView = document.createElement('div').setAttribute('id','field');
+    gameView.appendChild(fieldView);
+
+    // append selected card to field
+    const displayCardOnField = (card) => {
+        const field = document.getElementById('field');
+        field.appendChild(card);
+    };
+
     // button to end game
     document.getElementById('endGameButton').addEventListener('click', function () {
         game = false
     });
-
 
     async function drawPhase() {
         let handArr = await drawCards();
@@ -38,18 +50,12 @@ export default async function playGame() {
         document.querySelectorAll('.parentPlayerCardDiv').forEach((card) => {
             card.addEventListener('click', () => {
                 selectCard(card);
+                displayCardOnField(card);
             });
         });
     }
 
     drawPhase();
-    
-    
-    // create cards
-
-    // create select functionality
-
-    // once the cards are finished selecting, then we do handleAssignCards
 
     // battle itself
     
@@ -71,6 +77,4 @@ export default async function playGame() {
     while (game === 'battle') {
         
     }
-
-
 }
