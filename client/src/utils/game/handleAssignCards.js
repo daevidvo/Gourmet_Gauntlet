bcc10730044c850c0a7b7dd347038e84cdb73a34
David C <dchung20315@gmail.com>
2023-06-06 17:27:40 -0700

    handleAssignCards function

--- client/src/utils/game/handleAssignCards.js
+++ client/src/utils/game/handleAssignCards.js
@@ -1,4 +1,13 @@
-export const handleAssignCards = () => {
-    
-    
-}
+export const handleAssignCards = (handArr) => {
+    let fieldArray = [];
+
+    //need to edit to implement spices
+    const selectCard = (card) => {
+        if (fieldArray.length < 4) {
+            fieldArray.push(card);
+            handArr.splice(handArr.indexOf(card), 1);
+        }
+    }
+
+    return fieldArray;
+};
