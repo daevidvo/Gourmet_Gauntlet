e16066187de27690b253d181bf29139d87602a6a
daevidvo <itsdavidvo@gmail.com>
2023-06-06 18:47:33 -0700

    finished creating card elements function

--- client/src/utils/game/createCardElements.js
+++ client/src/utils/game/createCardElements.js
@@ -1,8 +1,48 @@
 export default function createCardElements (cardsArr, gameView) {
     
     cardsArr.forEach((data) => {
-        const div = document.createElement('div')
+        const parentCardDiv = document.createElement('div')
+        parentCardDiv.classList.add('card', 'w-4')
+
+        const cardImageDiv = document.createElement('div')
+        cardImageDiv.classList.add('card-image')
+
+        const cardImageFigure = document.createElement('figure')
+        cardImageFigure.classList.add('is-4by3')
+
+        const cardImage = document.createElement('img')
+        cardImage.setAttribute('src', data.cardImage)
+
+        const cardContentDiv = document.createElement('div')
+        cardContentDiv.classList.add('card-content')
+
+        const cardMediaDiv = document.createElement('div')
+        cardMediaDiv.classList.add('media')
+
+        const cardMediaContent = document.createElement('div')
+        cardMediaContent.classList.add('media-content')
+
+        const cardNamePTag = document.createElement('p')
+        cardNamePTag.classList.add('title', 'is-4')
+        cardNamePTag.textContent = data.cardName
         
-        
+        const cardHealthPTag = document.createElement('p')
+        cardHealthPTag.classList.add('subtitle', 'is-6')
+        cardHealthPTag.textContent = `Health: ${data.cardHealth}`
+
+        const cardAttackValue = document.createElement('div')
+        cardAttackValue.classList.add('content')
+        cardAttackValue.textContent = `Attack: ${data.cardAttack}`
+
+        cardImageFigure.append(cardImage)
+        cardImageDiv.append(cardImageFigure)
+
+        cardMediaContent.append(cardNamePTag, cardHealthPTag)
+        cardMediaDiv.append(cardMediaContent)
+        cardContentDiv.append(cardMediaDiv, cardAttackValue)
+
+        parentCardDiv.append(cardImageDiv, cardContentDiv)
+
+        gameView.append(parentCardDiv)
     })
 }
