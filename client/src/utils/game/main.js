2f909a8c13df3ca34bd466607466521e2cbea7dc
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-06-06 18:54:56 -0700

    trying to update game logic

--- client/src/utils/game/main.js
+++ client/src/utils/game/main.js
@@ -5,18 +5,46 @@ import createCardElements from "./createCardElements";
 
 export default async function playGame() {
     let game = 'draw'
+    let handArr = [];
 
     const gameView = document.getElementById('battle')
 
+    const startButton = document.createElement('button');
+    startButton.innerHTML = 'Start Battle';
+    startButton.addEventListener('click', startBattle);
+    gameView.appendChild(startButton);
+
+    // creates onclick for each card and displays card through jsx
+    // const createCardElement = (card) => (
+    //     <div id={card.id} onClick={() => selectCard(card)}>
+
+    //     </div>
+    // );
+
+    // once card is selected it is removed from handArr and added to fieldArr
+    const selectCard = (card) => {
+        handleAssignCards(handArr, card);
+        const selectedCard = document.getElementById(card.id);
+        selectedCard.remove();
+    };
+
+    
+
+    // button to switch to battle phase
+    const startBattle = () => {
+        game = "battle";
+    };
+
+    // button to end game
     document.getElementById('endGameButton').addEventListener('click', function () {
         game = false
-    })
+    });
 
 
     while (game === 'draw') {
     let handArr = await drawCards();
-
-    createCardElements(handArr, gameView)
+    const cardElements = handArr.map((card) => createCardElement(card));
+    ReactDOM.render(cardElements, gameView);
     
     // create cards
 
