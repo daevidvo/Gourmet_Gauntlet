fd606aed6f343e98db1c736ed62d8b4430de9991
daevidvo <itsdavidvo@gmail.com>
2023-06-09 23:49:15 -0700

    reverted connection.js changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect( 'mongodb://127.0.0.1:27017/GG_DB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/GG_DB');
 
 module.exports = mongoose.connection;
