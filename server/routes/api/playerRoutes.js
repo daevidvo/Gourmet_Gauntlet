e0f767ccce4a5601007fba8cdd69f0479077017c
daevidvo <itsdavidvo@gmail.com>
2023-06-10 22:17:57 -0700

    added increment functionality to user's stats on game win or loss

--- server/routes/api/playerRoutes.js
+++ server/routes/api/playerRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { postPlayerStats, putPlayerStats } = require('../../controllers/playerController');
+const { putPlayerStats } = require('../../controllers/playerController');
 
 router.route('/').put(putPlayerStats);
 
